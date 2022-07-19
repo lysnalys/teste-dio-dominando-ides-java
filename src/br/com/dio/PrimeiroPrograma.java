17cc8f04bf7575dc8f94a9c45a420a5c95222a80
Lysna <106987374+lysnalys@users.noreply.github.com>
2022-07-19 19:42:17 -0300

    Update PrimeiroPrograma.java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,14 +1,23 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 3;
+		Gato gato = new Gato();
+		Livros livros = new Livros();
 		
-		System.out.println("Hello World " + (a+b));
-		
+		System.out.println(gato);
+		System.out.println(livros);
+
 
 	}
 
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
 }
